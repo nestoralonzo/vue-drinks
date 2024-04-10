46b55f43ec90f0ccd826473c4a736ece65315742
Nestor Alonzo <nestoralonzo@hotmail.com>
2024-04-10 15:08:18 -0500

    Create drinks store

---
+++ src/stores/Drinks.js
@@ -0,0 +1,11 @@
+import { ref } from 'vue'
+import { defineStore } from "pinia"
+
+export const useDrinksStore = defineStore('drinks', () {
+
+    const categories = ref([])
+
+    return {
+        categories
+    }
+})
